d7a8ba87c5e05304362c8718561eeed6af23a324
juanngln <juanjonathan05@gmail.com>
2024-03-15 13:16:31 +0700

    Praktikum 6 Bagian A

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/list_barang', function () {
+    return view('layout/list');
 });
