c153f05000e533dca45cd2bc1a2d9115c5b7f5c7
juanngln <juanjonathan05@gmail.com>
2024-05-31 10:34:46 +0700

    Praktikum 11

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produk;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function index()
+    {
+        $numbers = range(1, 100);
+        return response()->json($numbers);  
+    }
+
+    public function getProduk() 
+    {
+        $data = Produk::all();
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
